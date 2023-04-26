854ef671989aa392336aa1bd176795891f55d4ee
utkarsh <gutkarsh191@gmail.com>
2023-04-26 23:42:33 +0530

    pipeline is updated

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,38 +1,38 @@
-const routes = require("express").Router()
+const routes = require("express").Router();
 
-routes.post('/', async (req, res) => {
-    const { searchTerm } = req.body;
-  
-    try {
-      const ads = await ad.aggregate([
-        {
-          $match: {  
-            $or: [
-              {'company.name': { $regex: searchTerm, $options: "i" } },
-              { primaryText: { $regex: searchTerm, $options: 'i' }},
-              { headline: { $regex: searchTerm, $options: 'i' } },
-              { description: { $regex: searchTerm, $options: 'i' } },
-            ],
-          },
+routes.post("/", async (req, res) => {
+  const { searchTerm } = req.body;
+
+  try {
+    const ads = await ad.aggregate([
+      {
+        $lookup: {
+          from: "companies",
+          localField: "companyId",
+          foreignField: "_id",
+          as: "company",
         },
-        {
-          $lookup: {
-            from: 'companies',
-            localField: 'companyId',
-            foreignField: '_id',
-            as: 'company',
-          },
+      },
+      {
+        $unwind: "$company",
+      },
+       {
+        $match: {
+          $or: [
+            {"company.name": { $regex: searchTerm, $options: "i" },},
+            { primaryText: { $regex: searchTerm, $options: "i" } },
+            { headline: { $regex: searchTerm, $options: "i" } },
+            { description: { $regex: searchTerm, $options: "i" } },
+          ],
         },
-        {
-          $unwind: '$company',
-        },
-      ]);
-  
-      res.status(200).json(ads);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
-    }
-  });
-  
-  module.exports = routes
+      },
+    ]);
+
+    res.status(200).json(ads);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = routes;
